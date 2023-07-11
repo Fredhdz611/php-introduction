0c3b2757fef0915eb5304c131a945f42631217cf
Alfredo Hdz <a1ahl611@gmail.com>
2023-07-11 12:18:16 -0600

    Added variables info

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,22 @@
 <?php include 'includes/header.php';
 
+$nombre = 'Alfredo '; //forma de declarar una variable.
+$_nombre = 'Alfredo '; //forma de declarar una variable.
+$nombre_ = 'Alfredo '; //forma de declarar una variable.
 
+
+echo $nombre;
+echo $_nombre;
+echo $nombre_;
+
+var_dump($nombre);
+
+define('constante', 'Valor de la constante '); //forma de declarar una constante, es una constante, pero no crea una variable, crea un identificador que es conocido como constante. 
+echo constante;
+
+//forma tradicional de declarar una const 
+const constante2 = 'constante dos';
+echo constante2;
 
 
 include 'includes/footer.php';

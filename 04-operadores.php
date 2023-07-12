dc2046923efa4ecf00948fa9e6e48d950d8ee410
Alfredo Hdz <a1ahl611@gmail.com>
2023-07-12 17:44:37 -0600

    mathematical operations

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+$num1 = 10;
+$num2 = 20;
 
+//suma
+echo $num1 + $num2;
+echo '<br>';
+
+//resta
+echo $num2 - $num1;
+echo '<br>';
+
+//multiplicacion
+echo $num1 * $num2;
+echo '<br>';
+
+//division 
+echo $num1 / $num2;
+echo '<br>';
+
+//Multiplicar cierta cantidad de veces 
+echo $num1 ** 3;
 
 
 include 'includes/footer.php';

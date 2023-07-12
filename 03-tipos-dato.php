1cba482f93b02bb6f962fd0908ef585306ca6056
Alfredo Hdz <a1ahl611@gmail.com>
2023-07-12 12:15:09 -0600

    Data types

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
 
+//boleano
+$logueado = true;
+var_dump($logueado);
+
+//enteros
+$num = 200;
+var_dump($num);
+
+//floats
+$float = 100.8;
+var_dump($float);
+
+//string
+$tex = 'Algo';
+var_dump($tex);
+
+//array
+$array = [];
+var_dump($array);
+
 
 
 include 'includes/footer.php';

095b430d42abad343fdf60f6a5ae2ac8f8129377
Alfredo Hdz <a1ahl611@gmail.com>
2023-07-12 18:00:11 -0600

    Inc and dec

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,5 +1,19 @@
 <?php include 'includes/header.php';
 
+$num1 = 20;
+echo ++$num1; //primero incrementa y luego imprime
+echo '<br>';
+
+$num2 = 20;
+echo $num2++; //imprime el valor actual y luego incrementa
+echo '<br>';
+
+$num3 = 20;
+echo --$num3; //primero decrementa y luego imprime
+echo '<br>';
+
+$num4 = 20;
+echo $num4--; //imprime el valor actual y luego decrementa
 
 
 

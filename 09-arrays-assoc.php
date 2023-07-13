fa82d275e3ae5f8e4ba844d59b746a9c37b2c3e6
Alfredo Hdz <a1ahl611@gmail.com>
2023-07-12 21:46:45 -0600

    added info

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+//vendria a ser un obj en JS
+$cliente = [
+    'nombre' => 'Juan', 
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium', 
+        'disponible' => 100
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente['informacion']);
+echo "</pre>";
+
+echo "<br>";
+echo $cliente['nombre'];
+
+echo "<br>";
+echo $cliente['informacion']['disponible'];
+$cliente['codigo'] = 1209192012;
+
+echo "<br>";
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
 include 'includes/footer.php';
